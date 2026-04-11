81f48aab9450a0ddc818cff303a6e39a3524e9fd
zhangjian <350010981@qq.com>
2026-04-11 21:58:05 +0800

    update: 项目文案调整

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -51,9 +51,7 @@ export const Projects = () => {
           />
           <h3> 图分析系统</h3>
           <p style={{ wordBreak: 'break-all' }}>
-            基于Mysql/CK/Elasticsearch/Kafka/Oracle/
-            SqlServer/PostgreSQL/syslog/txt/xlsx/csv等数据源的网络数据关系图分析系统，支持实时数据可视化分析和预警。
-            支持关系图节点、关系、属性、样式、预警样式等模型配置。
+          独立负责并完成项目调研、开发、自测、部署、交付、bug修复、版本迭代、性能优化及相关专利申请。实现了 MySQL、Kafka、xlsx文件等十余种数据源的数据关系图展示，多种图布局、聚类切换、路径查找分析。实现了自定义图模型配置管理、动态动画样式配置、图数据状态管理(React + Vite + TypeScript + G6.js + websocket + SQL)
           </p>
           <div className="project-tech">
             <span>React</span>
@@ -76,9 +74,8 @@ export const Projects = () => {
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.2 }}
           />
-          <h3>业务数据构建平台</h3>
-          <p>
-            一个数据源构建、业务数据构建、业务数据集模型配置、行为模型配置分析数据集共享、血缘关系可视化、系统日志、操作日志、业务数据日志、数据溯源的配置管理平台。
+          <h3>数据构建平台</h3>
+          <p style={{wordBreak: 'break-all'}}>负责项目前端核心模块的设计开发，部分node后端模块的设计开发；最终搭建成一套企业级规范数据构建和持久管理流程。实现了MySQL、Kafka、各类文件、Syslog多数据源的接入管理、原始/配置/资源/业务库等数据集的管理、分权共享能力。实现了数据源经模型、直连/同步任务到具体业务场景数据输出全程血缘追踪与统计大屏；标准字段、标准枚举管理。
           </p>
           <div className="project-tech">
             <span>React</span>
@@ -101,15 +98,18 @@ export const Projects = () => {
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.2 }}
           />
-          <h3>两客一危监管平台</h3>
+          <h3>应用构建平台</h3>
           <p>
-            一个支持多分屏实时监控查看、车辆定位、视频流回放、超速预警、疲劳驾驶预警...等数十种预警功能。
-            支持语音对讲、实时监管通知发送、设备管理、各种统计报表生成、大屏展示等省级监管平台。
+            负责应用可视化页面管理的开发维护。<br />
+            实现了用户通过拖拽左侧工具栏各种UI组件来构建编辑自定义页面<br />
+            通过数据平台的数据集表及其字段的维度指标来构造表格和分析图表<br />
+            实现了通过当前页面数据和iframe在当页直接展示图分析/沙箱等外部系统数据可视化组件的能力
           </p>
           <div className="project-tech">
             <span>Vue</span>
-            <span>TypeScript</span>
+            <span>Echarts</span>
             <span>Electron</span>
+            <span>Antv</span>
           </div>
         </motion.div>
       </motion.div>
